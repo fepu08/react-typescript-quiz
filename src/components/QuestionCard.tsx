e132fb190bd6cc7bffb994e1dd374ca1939d87b7
fepu08 <ferencbesenyei@protonmail.com>
2021-03-18 12:21:33 +0100

    fix types

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import { AnswerObject } from "../App";
 
-const QuestionCard = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+type Props = {
+  question: string;
+  answers: string[];
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
+  questionNr: number;
+  totalQuestions: number;
+};
 
-export default QuestionCard
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions
+}) => {
+  return (
+    <div>
+      <p className='number'>
+        Question: {questionNr} / {totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }}></p>
+      <div>
+        {answers.map((answer) => (
+          <div key={answer}>
+            <button
+              disabled={userAnswer ? true : false}
+              onClick={callback}
+              value={answer}
+            >
+              <span dangerouslySetInnerHTML={{ __html: answer }} />
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default QuestionCard;
