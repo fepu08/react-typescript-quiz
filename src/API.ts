5b0994fb58aca8fbcab743e64ecaedd068741a65
fepu08 <ferencbesenyei@protonmail.com>
2021-03-18 11:31:41 +0100

    set up api call

---
+++ src/API.ts
@@ -0,0 +1,37 @@
+import { shuffleArray } from "./utils";
+
+export type Question = {
+  category: string;
+  correct_answers: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+};
+
+export type QuestionState = Question & {
+  answerss: string[];
+};
+
+export enum Difficulty {
+  EASY = "easy",
+  MEDIUM = "medium",
+  HARD = "hard"
+}
+
+export const fetchQuizQuestions = async (
+  amount: number,
+  difficulty: Difficulty
+) => {
+  const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+  // first (inside) we await for fetch
+  // then (outside) we wait to convert it to json
+  const data = await (await fetch(endpoint)).json();
+  return data.results.map((question: Question) => ({
+    ...question,
+    answers: shuffleArray([
+      ...question.incorrect_answers,
+      question.correct_answers
+    ])
+  }));
+};
